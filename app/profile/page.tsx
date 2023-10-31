326c12ba2b371d00187bc5072079d02d1021d5d9
CandiceCaiYu <6caiyu6@gmail.com>
2023-10-31 15:24:30 +0800

    add profile slide1 content

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+import avatar from '../../public/avatar.jpg'
 import styles from './styles.module.scss'
+
 export default function profile() {
     return (
-        <div className={styles.profile}>profie</div>
+        <div className={styles.profile}>
+            <div className={`${styles.slide} ${styles.slide1}`}>
+                <div>
+                    <h2>Hello, I'm Yu Cai</h2>
+                    <h3>I'm | Front-end developer</h3>
+                </div>
+                <Image src={avatar} width={400} height={500} className={styles.img}/>
+            </div>
+        </div>
     )
 }
