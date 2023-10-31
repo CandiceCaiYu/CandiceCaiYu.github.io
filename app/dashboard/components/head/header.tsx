900e315294ab6fc78b978f491418be249013bd1f
CandiceCaiYu <6caiyu6@gmail.com>
2023-10-31 17:28:52 +0800

    transfer character

--- app/dashboard/components/head/header.tsx
+++ app/dashboard/components/head/header.tsx
@@ -4,15 +4,19 @@ import Image from "next/image";
 import styles from './styles.module.scss'
 import avatar from '../../../../public/avatar.jpg'
 import {useRouter} from "next/navigation";
+
 export default function Header() {
     const router = useRouter()
+
     function handleChange() {
         router.push('/profile')
     }
+
     return (
         <div className={styles.header}>
-            <h1>Cai Yu's Blog</h1>
-            <Image src={avatar} width={50}  height={50} className={styles.avatar} title={'contact me'} alt={'contact me'} onClick={handleChange}/>
+            <h1>Cai Yu&#39;s Blog</h1>
+            <Image src={avatar} width={50} height={50} className={styles.avatar} title={'contact me'} alt={'contact me'}
+                   onClick={handleChange}/>
         </div>
     )
 }
