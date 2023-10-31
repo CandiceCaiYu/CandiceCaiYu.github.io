86f34cb9b3e7c26f9d80eeea5cbef8dc1177aa5d
CandiceCaiYu <6caiyu6@gmail.com>
2023-10-31 16:27:42 +0800

    chore: update eslint config

--- app/profile/components/SlideAvator.tsx
+++ app/profile/components/SlideAvator.tsx
@@ -3,13 +3,13 @@ import avatar from "@/public/avatar.jpg";
 import Image from 'next/image'
 
 export default function SlideAvatar() {
-   return (
-       <div className={`${styles.slide} ${styles.slide1}`}>
-           <div>
-               <h2>Hello, I'm Yu Cai</h2>
-               <h3>I'm | Front-end developer</h3>
-           </div>
-           <Image src={avatar} width={400} height={500} className={styles.img}/>
-       </div>
-   )
+    return (
+        <div className={`${styles.slide} ${styles.slide1}`}>
+            <div>
+                <h2>Hello, I&#39;m Yu Cai</h2>
+                <h3>I&#39;m | Front-end developer</h3>
+            </div>
+            <Image src={avatar} width={400} height={500} className={styles.img} alt={'avatar'}/>
+        </div>
+    )
 }
