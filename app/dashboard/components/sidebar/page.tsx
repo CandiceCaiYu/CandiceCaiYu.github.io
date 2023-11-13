6558036ffa27f7fe75b6f2105dda19d57655b6f4
CandiceCaiYu <6caiyu6@gmail.com>
2023-11-13 10:30:38 +0800

    add notice section

--- app/dashboard/components/sidebar/page.tsx
+++ app/dashboard/components/sidebar/page.tsx
@@ -1,7 +1,12 @@
+import { Card } from 'antd'
 import styles from './styles.module.scss'
 
 export default function Sidebar() {
     return (
-        <div className={styles.sidebar}>side bar</div>
+        <div className={styles.sidebar}>
+            <Card title="Notice" className={styles.notice}>
+                <p>Freshman~</p>
+            </Card>
+        </div>
     )
 }
