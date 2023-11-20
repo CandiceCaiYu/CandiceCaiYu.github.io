e66322ab89fc2fb5763ac32f0391a711e906a6a1
CandiceCaiYu <6caiyu6@gmail.com>
2023-11-20 16:51:12 +0800

    [Dashboard][feature]: add categories in sidebar

--- app/dashboard/components/sidebar/page.tsx
+++ app/dashboard/components/sidebar/page.tsx
@@ -1,9 +1,9 @@
 import {Calendar, Card} from 'antd'
 import styles from './styles.module.scss'
 import React from 'react'
+import {SIDEBAR_CATEGORY} from '@/src/assets/constants'
 
 export default function Sidebar() {
-
     return (
         <div className={styles.sidebar}>
             <Card title="Notice" className={styles.notice}>
@@ -12,7 +12,14 @@ export default function Sidebar() {
             <div className={styles.wrapperStyle}>
                 <Calendar fullscreen={false}/>
             </div>
-            <div>Category</div>
+            <div>
+                <h2 className={styles.category_title}>Category</h2>
+                <ul>
+                    {SIDEBAR_CATEGORY.map(item => (
+                        <li key={item.name} className={styles.category_item}>{item.name}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
---
+++ src/assets/constants.ts
@@ -0,0 +1,7 @@
+export const SIDEBAR_CATEGORY = [
+    {name: 'React', link: ''},
+    {name: 'Javascript', link: ''},
+    {name: 'HTML', link: ''},
+    {name: 'CSS', link: ''},
+    {name: 'Typescript', link: ''},
+]
