20b2bd4357dffd78809ce7861db5431018eb57f7
CandiceCaiYu <6caiyu6@gmail.com>
2023-11-20 14:39:48 +0800

    [Dashboard][feature]: add calendar in sidebar

--- app/dashboard/components/sidebar/page.tsx
+++ app/dashboard/components/sidebar/page.tsx
@@ -1,12 +1,18 @@
-import { Card } from 'antd'
+import {Calendar, Card} from 'antd'
 import styles from './styles.module.scss'
+import React from 'react'
 
 export default function Sidebar() {
+
     return (
         <div className={styles.sidebar}>
             <Card title="Notice" className={styles.notice}>
                 <p>Freshman~</p>
             </Card>
+            <div className={styles.wrapperStyle}>
+                <Calendar fullscreen={false}/>
+            </div>
+            <div>Category</div>
         </div>
     )
 }
