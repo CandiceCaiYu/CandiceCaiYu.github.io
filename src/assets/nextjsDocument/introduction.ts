3279888ad2858c81dbeb97a99af15383890c18cf
CandiceCaiYu <6caiyu6@gmail.com>
2024-10-14 17:08:38 +0800

    Docs: add nextjs introduction text

---
+++ src/assets/nextjsDocument/introduction.ts
@@ -0,0 +1,36 @@
+export const welcome = "欢迎来到nextjs文档";
+export const concept1 = "Next.js是一个用于构建全栈Web应用程序的React框架。你可以用React组件去构建用户界面，并用Next.js实现附加功能和优化。";
+export const concept2 = "在底层，Next.js 还抽象并自动配置了React所需的工具，比如打包， 编译，等等。这样你就可以专注于构建你的应用程序， 不用花时间在配置应用上。";
+export const concept3 = "不管你是个人开发者还是大型团队中的一部分，Next.js可以帮助你构建交互式，动态且快速的react应用程序。";
+export const mainFeaturesTitle = "Next.js的一些主要的功能包括：";
+export const mainFeaturesRouting = "建立在服务器组件之上的基于文件系统的路由器，它支持布局，嵌套布局，加载状态，错误处理等。";
+export const mainFeaturesRendering = "使用客户端组件和服务器组件对客户端和服务器进行渲染，使用Next.js在服务器上进一步优化静态和动态渲染。在Edge和Node.js运行时进行流式传输。";
+export const mainFeaturesDataFetching = "使用服务器组件中的async/await简化了数据获取，并扩展的fetch API， 用于请求记忆化，数据缓存和重新验证。";
+export const mainFeaturesStyling = "支持你喜欢的样式方法，包括css module, Tailwind CSS, and CSS-in-JS";
+export const mainFeaturesOptimizations = "通过优化图片，字体，脚本来提高你应用程序的核心Web功能的生命力和用户体验。";
+export const mainFeaturesTypescript = "改善了对Typescript的支持, 提供更好的类型检查和更高效的编译，以及自定义Typescript插件和类型检查器。";
+
+
+export const howToUseDocsTitle = '如何使用这些文档';
+export const howToUseDocsLeft = '在屏幕的左边，你可以看到文档的导航栏。 这些文档页面都是按顺序排列的。从基础到高级，所以你在构建应用的时候，可以跟着文档一步一步的来。不过，你可以从任意顺序阅读它们，也可以直接跳到你想要应用的页面';
+export const howToUseDocsRight = '在屏幕的右边，你可以看到一个目录，它可以让你更容易在这个页面的各个章节之间导航。如果你想要更快的找到一个页面，你可以使用页面顶部的搜索栏，也可以使用搜索快捷键（ctrl + K或者command + K）\n';
+export const howToUseDocsEnd = '开始前， 请切换到安装引导页。';
+
+
+export const routerTitle = '应用程序路由器 VS 页面路由器';
+export const routerSection1 = 'Next.js有两种不同的路由器：应用程序路由器和页面路由器。应用程序路由器是一个较新的路由器。它允许你使用react最新的功能，比如服务器组件和流式传输。页面路由器是Next.js原本的路由器，它允许你构建服务端渲染的react应用程序并且继续支持旧的Next.js应用程序';
+export const routerSection2 = '在侧边栏的顶部，你可以看到一个下拉菜单，它可以让你在应用程序路由器和页面路由器的功能之间进行切换。这两个路由器的功能都是独有的，所以跟踪你的选项卡很重要。';
+export const routerSection3 = '页面顶部的面包屑同样会指示你当前正在查看的是应用程序路由器还是页面路由器的文档。';
+
+
+export const preRequireTitle = '必备知识';
+export const preRequireSection1 = '尽管我们的文档设计的对初学者很友好，但我们需要建议一个基准，这样文档可以更加关注于Next.js的功能。每当我们引入一个新的概念时，我们会提供相关文档的链接。';
+export const preRequireSection2 = '为了充分利用我们的文档，建议你对HTML，CSS， 和React有个基本的了解。如果你想提高你的react技能，请查看react基础原理课程，它可以向你介绍基础原理。然后通过构建一个仪表盘应用程序来学习更多Next.js的知识。';
+
+
+export const accessibilityTitle = '可访问性';
+export const accessibilityContent = '为了在使用屏幕阅读器阅读文档时获得最佳的可访问性，我们推荐使用Firefox 和NVDA, 或者Safari 和 VoiceOver。';
+
+
+export const communityTitle = "加入我们的社区。";
+export const communityContent = "如果你有任何关于Next.js的问题，欢迎来我们的社区 GitHub Discussions, Discord, Twitter, and Reddit提问";
