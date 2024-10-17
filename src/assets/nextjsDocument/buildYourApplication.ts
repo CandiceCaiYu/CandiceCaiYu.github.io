153e49cf0bd26c92ad4950679a0f01cc6c346c81
CandiceCaiYu <6caiyu6@gmail.com>
2024-10-17 15:28:05 +0800

    Docs: add building your application text

---
+++ src/assets/nextjsDocument/buildYourApplication.ts
@@ -0,0 +1,4 @@
+export const title = "构建你的应用程序";
+export const content1 = "Next.js提供构建块去创建一个灵活的，全栈的网络应用程序。在Buiilding Your Application手册里介绍了如何使用这些功能和如何自定义你的应用程序行为";
+export const content2 = "这些章节和页面是按顺利排列的，从基础到高级，当构建你的Next.js应用程序的时候，你可以跟着它们一步一的来。不过，你可以从任意地方开始阅读，或者直接跳到适用于你的用例的页面。";
+export const content3 = "如果你是一个Next.js新手，我们推荐你从Routing, Rendering, Data Fetching 和 Styling 章节开始，它们介绍基本的Next.js和web概念,可以帮助你入门。然后，你可以深入了解其他章节比如 Optimizing 和 Configuring。最后，如果你准备好了，就可以查看Deploying 和 Upgrading章节。";
