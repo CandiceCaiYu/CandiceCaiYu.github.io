ba1e1f7683041e1a767fff3cafab6b3fff63df93
Yu Cai <6caiyu6@gmail.com>
2025-08-18 10:18:38 +0800

    feat(reading-progress): add SelectedWorksOfMaoZedong1 component for data visualization

---
+++ app/reading-progress/components/SelectedWorksOfMaoZedong1/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+import {EChartsOption} from "echarts";
+
+export default function SelectedWorksOfMaoZedong1() {
+    const option: EChartsOption = {
+        title: {
+            text: '毛泽东选集1',
+            textAlign: 'left'
+        },
+        xAxis: {
+            type: 'category',
+            data: ['8-2', '8-17']
+        },
+        yAxis: {
+            type: 'value',
+            max: 340
+        },
+        series: [
+            {
+                data: [153, 183,],
+                type: 'line'
+            }
+        ]
+    };
+
+    return <ReactECharts option={option} style={{height: '500px', width: '500px',}}/>;
+}
--- app/reading-progress/page.tsx
+++ app/reading-progress/page.tsx
@@ -1,10 +1,16 @@
 import JavaThinking from "@/app/reading-progress/components/java-thinking";
 import styles from './styles.module.scss'
+import SelectedWorksOfMaoZedong1 from "@/app/reading-progress/components/SelectedWorksOfMaoZedong1";
+import {Flex} from "antd";
+
 export default function Page() {
     return (
         <div className={styles.readingProcess}>
             <h1>阅读进度图</h1>
-            <JavaThinking />
+            <Flex>
+                <JavaThinking/>
+                <SelectedWorksOfMaoZedong1/>
+            </Flex>
         </div>
     )
 }
