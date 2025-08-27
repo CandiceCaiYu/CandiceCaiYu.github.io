a1f96ab0f325e06131d99c3c0d101428792e2962
Yu Cai <6caiyu6@gmail.com>
2025-08-27 12:16:55 +0800

    feat(header): replace avatar with Home icon in HeaderBackToHome component

--- app/dashboard/components/head/headerBackToHome.tsx
+++ app/dashboard/components/head/headerBackToHome.tsx
@@ -1,17 +1,15 @@
 'use client'
 
-import Image from "next/image";
 import styles from './styles.module.scss'
-import avatar from '@/public/avatar.png'
 import {CSSProperties} from "react";
 import Link from "next/link";
+import {HomeTwoTone} from "@ant-design/icons";
 
 export default function HeaderBackToHome({style}: { style?: CSSProperties }) {
     return (
         <div className={styles.header} style={style}>
             <Link href={'/dashboard'}>
-                <Image src={avatar} width={60} height={72} className={styles.avatar} title={'contact me'}
-                       alt={'contact me'}/>
+                <HomeTwoTone className={styles.homeIcon}/>
             </Link>
         </div>
     )
